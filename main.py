bff8275c8a111954c48410df27c9c7d74ae08e28
3matt3 <mmatt333@protonmail.com>
2024-06-08 02:27:07 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('Hello World')
+
+# Link to database (SQLLite)
+# Scrape list of substances from tripsit fcatsheets
+# User can make entries for when they dose a substance and store in a DB (SQLite) (i.e '!dose meth 50mg)
+# User can set parameters like; No more than 1gram of meth per week etc.
+# User can see the history of their use in a given time period.
+# Warn User about drug interactions/dangers/suggest safer alternatives
+# User can fech information regarding a certain drug (similar to my bot ive alrdy made)
+
+# Start with CLI
+# Make a GUI
+# Make into a webapp usable on mobile too
+# Standalone mobile app
