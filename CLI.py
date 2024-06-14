7a173a6eaf128e90a5bf197b8f1ce7af2183c4e3
3matt3 <mmatt333@protonmail.com>
2024-06-14 16:44:06 +1000

    Fix History Function

--- CLI.py
+++ CLI.py
@@ -6,12 +6,9 @@ import json
 # sTORE DATA
 # Send warning for high doses and bad combos
 # Finish Scraping and parsing info!
-
-# 
+ 
 print('-----------------------------\n')
 print('Welcome to Substance Secrets. \n\nAn app with a focus on harm reduction by allowing recreational drug users the ability to track their usage,\nas well as the ability to easily find info for any substance')
-
-
 print('\n-----------------------------\n')
 print('To get a summary of a drug, type   !info drug   \n')
 print('To view our commands and functions, enter: !help \n')
@@ -30,14 +27,12 @@ except:
 try:
     with open('data.json') as h:
         data = json.load(h)
-        drugs_consumed_txt.append(data)
+        drugs_consumed_txt.extend(data)
     h.close()
 except:
     pass
 
 while True:
-
-
     command = input(': ')
 
     if command == '!help':
@@ -55,17 +50,13 @@ while True:
     if command == '!exit':
         break
 
-
     if command.startswith('!'):
-
         command2 = str(command.split('!')[1])
         try:
-         
             drug = str(command.split(' ')[1])
         except:
             drug = '' 
         cmd = command2.split(' ')[0]
-
 
         if cmd == 'info':
             summary = get_info(drug)
@@ -84,32 +75,33 @@ while True:
             route = input('How did you take '+drug+' ? (Oral,Snorted,Smoked,IV etc) \n')
             time = datetime.datetime.now()
             info = {'drug': drug, 'dosage': dosage, 'roa': route,'time': str(time)}
-
             msg = str(dosage+' mg of '+drug+' taken via '+route+' at: '+str(time))
-
-
 
             drugs_consumed.append(info)
             drugs_consumed_txt.append(msg)
 
             with open('history.json', 'w') as f:
-                json.dump(drugs_consumed, f)
-                
+                json.dump(drugs_consumed, f)   
             f.close()
-            
             
             with open('data.json', 'w') as h:
                 json.dump(drugs_consumed_txt, h)
-
             h.close()
 
+            print('\n\n')
             print(msg)
             print('\n\n')   
 
         elif cmd == 'history':
-
+            print('\n\n')
+            #print(len(drugs_consumed_txt[0]))
+            i = 0
             for x in drugs_consumed_txt:
                 print(x)
+            print('\n\n')
+                #while i < len(drugs_consumed_txt[0]):
+                #    print(str(x[i]))
+                #    i = i+1
                 
 
         
